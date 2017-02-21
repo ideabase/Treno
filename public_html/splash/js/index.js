69905befc33d846de8a408554b787623e67e2d51
Alan W <awalke59@kent.edu>
2017-02-20 19:26:08 -0500

    splash 2.0

--- public_html/splash/js/index.js
+++ public_html/splash/js/index.js
@@ -2,17 +2,17 @@
 /*basic */
 $(document).ready(function(){
     
-    $(window).bind('scroll',function(e){
-    parallaxScroll();
-});
-
-function parallaxScroll(){
-    var scrolled = $(window).scrollTop();
-    $('.Layer_1').css('top',(180-(scrolled*.5))+'px');
-    $('.Layer_2').css('top',(100-(scrolled*.5))+'px');
-    $('#Layer_3').css('top',(0-(scrolled*.5))+'px');
-}
-  
+//    $(window).bind('scroll',function(e){
+//    parallaxScroll();
+//});
+//
+//function parallaxScroll(){
+//    var scrolled = $(window).scrollTop();
+//    $('.Layer_1').css('top',(-(scrolled*.5))+'px');
+//    $('.Layer_2').css('top',(100-(scrolled*.5))+'px');
+//    $('#Layer_3').css('top',(0-(scrolled*.5))+'px');
+//}
+//  
 
 $("#logo").click(function(){
         $('html,body').animate({
