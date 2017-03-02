751e1d7b10816324ecd4e4d4cce71fec70696e12
Alex Longwell <alex_longwell@hotmail.com>
2017-03-02 12:21:22 -0500

    Gallery
    added gallery plugin. Changed header and body styling to match wireframes.

--- public_html/production/assets/js/scripts.js
+++ public_html/production/assets/js/scripts.js
@@ -1,1 +1,14 @@
-var hellooo;
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+  stagePadding: 50,
+  nav: true,
+  loop: true,
+  responsiveClass: true,
+  respnsive:{
+  	0:{
+  		items:1,
+  		nav: true,
+  		loop: true,
+  	}
+  }
+});
