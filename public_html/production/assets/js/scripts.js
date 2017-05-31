d897261f99a17fb81a95282ff9425d09876b7c07
Emily <emily@uncubed.com>
2017-05-31 13:08:36 -0400

    Article carousel
    added in js for article carousel

--- public_html/production/assets/js/scripts.js
+++ public_html/production/assets/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  $(".owl-carousel").owlCarousel({
+  $(".photo-gallery").owlCarousel({
 	  nav : true,
 	  loop: true,
 	  autoplay: true,
@@ -13,6 +13,23 @@ $(document).ready(function(){
 	  		items:1,
 
 	  		loop: true
+	  	}
+	  }
+  });
+
+  $(".article-carousel").owlCarousel({
+	  nav : true,
+	  autoplay: false,
+	  autoplayHoverPause: true,
+	  autoplayTimeout: 5000,
+	  autoplaySpeed: 1500,
+	  responsiveClass: true,
+	  items:3,
+	  loop:true,
+	  responsive:{
+	  	0:{
+	  		items:3,
+
 	  	}
 	  }
   });
